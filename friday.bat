19961d9aa56d9c1a976b88eb177cb84f8c978cc9
pedro.fabrino <pedro.fabrino@aisdigital.com.br>
2019-10-16 18:17:37 -0300

    Still fixing encoding issues

--- friday.bat
+++ friday.bat
@@ -1,23 +1,20 @@
 :while
-:: Set volume to maximum
-nircmd.exe setsysvolume 65535
-:: Speak to the user
-PowerShell -Command "Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('Vampetização rolando!');"
+REM :: Speak to the user
+PowerShell -Command "Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('Vampetização rolando!');"
 
-:: Download picture
-PowerShell -Command "(New-Object Net.WebClient).DownloadFile('https://i.pinimg.com/originals/38/0d/75/380d75bf5f3b84a9112eff7360c9a61a.jpg', 'C:/users/public/vampeta.jpg')"
+REM :: Download picture
+PowerShell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/PedroFabrino/friday/blob/master/vampeta.jpg?raw=true', 'C:/users/public/vampeta.jpg')"
 
-:: Set picture as desktop
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:/Users/Public/vampeta.jpg /f 
+REM :: Set picture as desktop
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallPaper /t REG_SZ /d C:/Users/Public/vampeta.jpg /f 
+timeout 2
 UpdatePerUserSystemParameters
 
-:: Set volume to maximum
-nircmd.exe setsysvolume 65535
-:: Speak to the user
-PowerShell -Command "Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('Vampetização finalizada, volte sempre');"
+REM :: Speak to the user
+PowerShell -Command "Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('Vampetização finalizada, volte sempre');"
 
-:: Wait 30 seconds
+REM :: Wait 30 seconds
 timeout 30
 
-:: Do it all over again :D
+REM :: Do it all over again :D
 goto :while
