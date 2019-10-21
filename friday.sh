45fbf3493c692bb6a0d1018e984218998363a72c
Pedro Fabrino <pfabrino@gmail.com>
2019-10-21 13:44:09 -0300

    Fixed (?) shell script

--- friday.sh
+++ friday.sh
@@ -13,48 +13,44 @@ count=1
 while true
 do
 
-  # Sleep
-
-  sleep $period
-
   osascript -e 'set volume output volume 100'
   say -v Luciana 'Sistema iniciando'
+  osascript -e 'set volume output volume 100'
   say -v Luciana 'Áreas vitais verificadas'
+  osascript -e 'set volume output volume 100'
   say -v Luciana 'Podemos começar...'
-
-  sleep $period
-
+  osascript -e 'set volume output volume 100'
   say -v Luciana 'Agora aquele vampetinho maroto.'
+  osascript -e 'set volume output volume 100'
+  
   # Download image
-
-  curl -s -o $image_file_name 'http://cultebol.com.br/wp-content/uploads/2014/01/vampeta-fala-sobre-ensaio-nu-para-a-g-magazine.jpg'
+  curl -s -o $image_file_name 'https://raw.githubusercontent.com/PedroFabrino/friday/master/vampeta.jpg'
 
   # Set Desktop background
   osascript -e 'set volume output volume 100'
   osascript -e 'tell application "System Events" to set picture of every desktop to (do shell script "pwd") & "/'$image_file_name'"'
 
   # Delete file
-
   rm $image_file_name
 
   # Say
-
   osascript -e 'set volume output volume 100'
   say -v Luciana 'Ciclo de Vampetização número' $count
 
   sleep $period
 
   osascript -e 'set volume output volume 100'
-  say -v Luciana 'Como assim flegy tentando se livrar de mim?'
+  say -v Luciana 'Como assim amigo, está tentando se livrar de mim?'
 
   sleep $period
-
+  
+  osascript -e 'set volume output volume 100'
   say -v Luciana 'Eu sei que você me ama!'
 
   sleep $period
 
   osascript -e 'set volume output volume 100'
-  say -v Luciana 'Fleggyyyy, queridoooo...'
+  say -v Luciana 'Ooooopa, queridoooo...'
 
   sleep $period
 
@@ -62,8 +58,8 @@ do
   say -v Luciana 'Por hoje é só velhinho...'
 
   sleep $period
-  # Increment
 
+  # Increment
   count=$((count+1))
 
 done
